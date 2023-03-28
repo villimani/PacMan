b3c0d7d7bf9a4297c4703d5db08e35a6ffeb41ef
villimani <vilmundurmani@gmail.com>
2023-03-28 12:22:49 +0000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module hopverkefni.vidmot {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens hopverkefni.vidmot to javafx.fxml;
+    exports hopverkefni.vidmot;
+}
