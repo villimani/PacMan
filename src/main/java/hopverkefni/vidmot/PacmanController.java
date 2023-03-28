c86c8b2fd29686e8dc9ddf77ab285675d36f456e
villimani <vilmundurmani@gmail.com>
2023-03-28 12:32:31 +0000

    Fyrsta commit með adal view

--- src/main/java/hopverkefni/vidmot/PacmanController.java
+++ src/main/java/hopverkefni/vidmot/PacmanController.java
@@ -3,7 +3,7 @@ package hopverkefni.vidmot;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class PacmanController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module hopverkefni.vidmot {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens hopverkefni.vidmot to javafx.fxml;
     exports hopverkefni.vidmot;
 }
