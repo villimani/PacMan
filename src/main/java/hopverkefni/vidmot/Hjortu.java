bce04e7c88c394c9e10f3e8fbb19ddae692fc6df
villimani <vilmundurmani@gmail.com>
2023-04-14 14:21:22 +0000

    bjo til hjörtuborð en setti pac mynd inn í staðin

---
+++ src/main/java/hopverkefni/vidmot/Hjortu.java
@@ -0,0 +1,9 @@
+package hopverkefni.vidmot;
+
+import javafx.scene.image.ImageView;
+
+public class Hjortu extends ImageView {
+    public Hjortu() {
+        FXML_Lestur.lesa(this, "Hjortu.fxml");
+    }
+}
---
+++ src/main/java/hopverkefni/vidmot/HjortuBord.java
@@ -0,0 +1,11 @@
+package hopverkefni.vidmot;
+
+import javafx.scene.layout.Pane;
+
+public class HjortuBord extends Pane {
+
+    public HjortuBord() {
+        FXML_Lestur.lesa(this, "hjortun.fxml");
+    }
+}
+
