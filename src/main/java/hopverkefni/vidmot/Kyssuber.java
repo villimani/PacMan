3ed144f12d3017a31958d26822a8024e39211e4e
villimani <vilmundurmani@gmail.com>
2023-04-21 16:29:40 +0000

    setti kyssuber inn

---
+++ src/main/java/hopverkefni/vidmot/Kyssuber.java
@@ -0,0 +1,10 @@
+package hopverkefni.vidmot;
+
+import javafx.scene.image.ImageView;
+
+public class Kyssuber extends ImageView {
+
+    public Kyssuber() {
+        FXML_Lestur.lesa(this, "Kyssuber.fxml");
+    }
+}
